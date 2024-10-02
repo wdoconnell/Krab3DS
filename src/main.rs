189c9497bf025d41088665b6c2e4c754701db967
Bill O'Connell <91283923+wdoconnell@users.noreply.github.com>
2024-10-01 23:34:51 -0400

    feat: partially working, albeit stuttery audio

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,45 @@
 const VID_3DS: u16 = 0x16D0;
 const PID_3DS: u16 = 0x06A3;
+use minifb::Scale;
+use minifb::ScaleMode;
+use minifb::WindowOptions;
+use rodio::OutputStreamHandle;
+use rodio::{OutputStream, Source};
 use std::time::Duration;
 use std::time::SystemTime;
 
 use rusb::{Device, DeviceHandle, GlobalContext};
 // Might be a better way to specify this
 // const BULK_ENDPOINT_ADDRESS: u8 = 130;
-const DEFAULT_TIMEOUT: Duration = Duration::from_millis(500);
+// This might break stuff if we drop below 10 fps
+const DEFAULT_TIMEOUT: Duration = Duration::from_millis(100);
 const VEND_OUT_REQ: u8 = 0x40;
 const VEND_OUT_VALUE: u16 = 0;
 const VEND_OUT_IDX: u16 = 0;
 
+const VIDEO_WIDTH: usize = 240;
+const VIDEO_HEIGHT: usize = 720;
+const RGB_COLOR_SIZE: usize = 3;
+const VIDEO_BUFFER_SIZE: usize = VIDEO_WIDTH * VIDEO_HEIGHT * RGB_COLOR_SIZE;
+
+// Still figuring out the exact size of the audio buffer.
+const REQUIRED_BUFFER_SIZE: usize = 1916;
+const AUDIO_BUFFER_SIZE: usize = 2188 + REQUIRED_BUFFER_SIZE;
+const AUDIO_SAMPLE_HZ: u32 = 32728;
+// const AUDIO_SAMPLE_HZ: u32 = 44100;
+// const AUDIO_SAMPLE_HZ: u32 = 22050;
+
 // Could give more specific explanation here.
 // const FULL_BUFF_SIZE: usize = 522500;
-const FULL_BUFF_SIZE: usize = 523500;
-const VIDEO_BUFFER_SIZE: usize = 518400;
-// const EXTENDED_VID_BUFFER_SIZE: usize = 550000;
+// Is there a way to make it dynamic?
+// const FULL_BUFF_SIZE: usize = 523500;
+const FULL_BUFF_SIZE: usize = VIDEO_BUFFER_SIZE + AUDIO_BUFFER_SIZE;
+// This is just an initial value when window can be resized.
+const WINDOW_HEIGHT: usize = 240;
+const WINDOW_WIDTH: usize = 720;
+
+// Could change this if we ultimtely exceed 60 fps
+const TARGET_FPS: usize = 60;
 
 #[derive(Debug, Clone)]
 struct Endpoint {
@@ -44,6 +68,32 @@ struct DS {
 impl DS {
     pub fn new(device: Device<GlobalContext>, handle: DeviceHandle<GlobalContext>) -> Self {
         Self { device, handle }
+    }
+}
+
+struct CustomWindowOptions {
+    opts: WindowOptions,
+}
+
+impl CustomWindowOptions {
+    pub fn new(borderless: bool, resize: bool, scale: Scale, scale_mode: ScaleMode) -> Self {
+        Self {
+            opts: WindowOptions {
+                borderless,
+                resize,
+                scale,
+                scale_mode,
+                none: false,
+                title: true,
+                topmost: false,
+                transparency: false,
+            },
+        }
+    }
+
+    // Should this be here or trait impl?
+    pub fn inner(&self) -> WindowOptions {
+        self.opts
     }
 }
 
@@ -119,7 +169,7 @@ fn capture_screen(
     handle: &DeviceHandle<GlobalContext>,
     endpoint: &Endpoint,
     window: &mut minifb::Window,
-    // sink: &mut rodio::Sink,
+    audio_handle: &OutputStreamHandle,
 ) {
     // Should be able to clean this up so we don't need to duplicate the buffer.
     // let max_buffer_size = 525000;
@@ -132,6 +182,7 @@ fn capture_screen(
     );
 
     // Probably need to handle this because it could fail on any vend
+    // I guess we need to do this each time?
     handle
         .write_control(
             vend_out_req_type,
@@ -145,6 +196,7 @@ fn capture_screen(
 
     let mut combined_buff = vec![0u8; FULL_BUFF_SIZE];
 
+    // Explore options to reduce input lag?
     loop {
         match handle.read_bulk(endpoint.address, &mut combined_buff, DEFAULT_TIMEOUT) {
             Ok(bytes_rec) => {
@@ -159,20 +211,35 @@ fn capture_screen(
         }
     }
 
-    // 50% framerate improvement!
-    let (vid_buff, _audio_buff) = combined_buff
+    // This needs to be moved up a level.
+    let (vid_buff, remainder) = combined_buff
         .split_first_chunk::<VIDEO_BUFFER_SIZE>()
         .expect("couldnt break buffers");
+
+    let (audio_buff, info_buff) = remainder
+        .split_first_chunk::<AUDIO_BUFFER_SIZE>()
+        .expect("couldnt break audio buffer");
 
     let width = 240;
     let height = 720;
 
+    // Should make this a constant
+    let f32_sample: Vec<f32> = audio_buff
+        .iter()
+        .map(|&sample| (sample as f32 - 128.0) / 128.0)
+        .collect();
     // Wonder if there's a way we can avoid the rotation. Or continue building the buffer without blocking the draw.
+
     let vid_buf_32 = u8_to_u32(vid_buff);
     let rotated_vid_buf = rotate_270(&vid_buf_32, width, height);
     window
         .update_with_buffer(&rotated_vid_buf, height, width)
         .unwrap();
+
+    let audio_src = rodio::buffer::SamplesBuffer::new(2, AUDIO_SAMPLE_HZ, f32_sample);
+    audio_handle
+        .play_raw(audio_src.convert_samples())
+        .expect("couldnt play audio");
 }
 
 // ROTATING CODE
@@ -234,22 +301,32 @@ fn main() {
         .set_alternate_setting(endpoint.iface, endpoint.setting)
         .unwrap();
 
-    // This can be made arbitrarily large as long as it's a multiple of 720x240.
+    let window_options =
+        CustomWindowOptions::new(true, true, Scale::X2, ScaleMode::AspectRatioStretch);
+
     let mut window =
-        minifb::Window::new("Test", 1440, 480, minifb::WindowOptions::default()).unwrap();
+        minifb::Window::new("Test", WINDOW_WIDTH, WINDOW_HEIGHT, window_options.inner()).unwrap();
 
     let start_time = SystemTime::now();
     let mut fps: u64 = 0;
     let mut last_reported_sec: f32 = 0.0;
 
+    let (_audio_stream, audio_stream_handle) =
+        OutputStream::try_default().expect("couldnt create output stream");
+
+    // Way to improve this might be as follows:
+    // Loop through data from device as quickly as possible and return video, audio.
+    // Queue video and audio into buffers.
+    // Have separate method that sends out smooth synced video/audio.
+    // TLDR Audio/video syncing is hard.
+
     // See https://docs.rs/minifb/latest/i686-pc-windows-msvc/src/minifb/lib.rs.html#533-535
-    window.set_target_fps(60);
-
-    // This improved framerates up to about 30fps.
+    window.set_target_fps(TARGET_FPS);
+    // This improved framerates up to the cap of 30fps on most games
+    // Would be nice to find something with 60fps to test.
     while window.is_open() && !window.is_key_down(minifb::Key::Escape) {
-        capture_screen(&ds.handle, &endpoint, &mut window);
+        capture_screen(&ds.handle, &endpoint, &mut window, &audio_stream_handle);
 
-        // Framerate reporting
         fps += 1;
         let current_time = SystemTime::now();
         let elapsed_secs = current_time
